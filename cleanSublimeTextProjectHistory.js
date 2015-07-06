1718d185bd5a83c559558ac4a97b03e1cd23f93b
br4nnigan <nils.poltoraczyk@bayerundpreuss.com>
2015-07-06 13:31:48 +0200

    also checking if project file exists

--- cleanSublimeTextProjectHistory.js
+++ cleanSublimeTextProjectHistory.js
@@ -1,39 +1,64 @@
 var osenv = require('osenv')
 var home = osenv.home();
 var fs = require("fs");
-var filepath = home+"/Library/Application Support/Sublime Text 3/Local/Session.sublime_session";
+var session_file_path = home+"/Library/Application Support/Sublime Text 3/Local/Session.sublime_session";
 
 
-fs.readFile(filepath, "binary", function(err, file) {
+fs.readFile(session_file_path, "binary", function(err, session_file) {
 
 	if(err) {
 		console.log( err + "\n" );
 	}
 	else{
 
-		file = file.replace( /\t/g,  '', "g"); // escape tabs
+		session_file = session_file.replace( /\t/g,  '', "g"); // escape tabs
 
-		var sublime_session = JSON.parse(file)
-		var lengthBefore = sublime_session.workspaces.recent_workspaces.length;
-		var lengthAfter;
+		var
+		sublime_session = JSON.parse(session_file),
+		lengthBefore    = sublime_session.workspaces.recent_workspaces.length,
+		lengthAfter;
 
-		sublime_session.workspaces.recent_workspaces = sublime_session.workspaces.recent_workspaces.filter(function (path) {
 
-			if ( !fs.existsSync(path) ){
-				console.log('Path does not exist:', path);
+
+		sublime_session.workspaces.recent_workspaces = sublime_session.workspaces.recent_workspaces.filter(function (workspace_file_path) {
+
+			if ( !fs.existsSync(workspace_file_path) ){
+				console.log('Workspace does not exist:', workspace_file_path);
 				return false;
-			}else{
+			}else {
+
+				var
+				workspace,
+				project_file_path,
+				workspace_file = fs.readFileSync(workspace_file_path, {encoding: "utf8"});
+
+				if ( workspace_file ){
+
+					workspace_file    = workspace_file.replace( /\t/g,  '', "g"); // escape tabs
+					workspace         = JSON.parse(workspace_file);
+					project_file_path = workspace_file_path.substr(0, workspace_file_path.lastIndexOf("/")) + "/" + workspace.project;
+
+					if ( workspace.project && !fs.existsSync(project_file_path) ){
+						console.log('Project does not exist:', project_file_path);
+						return false;
+					}else{
+						// console.log('Project DOES exist:', project_file_path);
+					}
+				}
+
 				return true;
 			}
 		});
+
+
 
 		lengthAfter = sublime_session.workspaces.recent_workspaces.length
 
 		if ( lengthAfter != lengthBefore ){
 
-			file = JSON.stringify(sublime_session);
+			session_file = JSON.stringify(sublime_session);
 
-			fs.writeFile(filepath, file, function (err) {
+			fs.writeFile(session_file_path, session_file, function (err) {
 				if (err)
 					throw err;
 				else
